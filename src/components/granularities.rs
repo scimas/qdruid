b43292f4239d63bf0510c3ed3218e9e28a384818
Mihir Gadgil <mmgadgil20@gmail.com>
2021-10-28 23:14:52 -0600

    Implement granularities

---
+++ src/components/granularities.rs
@@ -0,0 +1,52 @@
+pub enum Granularity {
+    Simple(String),
+    Duration {
+        duration: u64,
+        origin: Option<String>,
+    },
+    Period {
+        period: String,
+        origin: Option<String>,
+        time_zone: Option<String>,
+    },
+}
+
+impl Granularity {
+    fn simple(granulaity: &str) -> Self {
+        Self::Simple(granulaity.into())
+    }
+
+    fn duration(duration: u64) -> Self {
+        Self::Duration {
+            duration,
+            origin: None,
+        }
+    }
+
+    fn duration_with_origin(duration: u64, origin: &str) -> Self {
+        Self::Duration {
+            duration,
+            origin: Some(origin.into()),
+        }
+    }
+
+    fn period(period: &str) -> Self {
+        Self::Period {
+            period: period.into(),
+            origin: None,
+            time_zone: None,
+        }
+    }
+
+    fn period_with_origin_tz(
+        period: &str,
+        origin: Option<String>,
+        time_zone: Option<String>,
+    ) -> Self {
+        Self::Period {
+            period: period.into(),
+            origin,
+            time_zone,
+        }
+    }
+}
--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,3 +1,4 @@
 mod druid_types;
 pub mod data_sources;
 pub mod intervals;
+pub mod granularities;
