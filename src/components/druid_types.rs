90b1adcf51b578445088086bed9d65e4b86ad9b5
Mihir Gadgil <mmgadgil20@gmail.com>
2021-10-28 21:27:50 -0600

    Implement druid types

--- src/components/druid_types.rs
+++ src/components/druid_types.rs
@@ -1,7 +1,29 @@
-pub enum DruidType {
+pub enum DruidType<C> {
     String(String),
     Double(f64),
     Float(f32),
     Long(i64),
-    Complex,
+    Complex(C),
+}
+
+impl<C> DruidType<C> {
+    fn string<T: Into<String>>(s: T) -> Self {
+        Self::String(s.into())
+    }
+
+    fn double<T: Into<f64>>(d: T) -> Self {
+        Self::Double(d.into())
+    }
+
+    fn float<T: Into<f32>>(f: T) -> Self {
+        Self::Float(f.into())
+    }
+
+    fn long<T: Into<i64>>(l: T) -> Self {
+        Self::Long(l.into())
+    }
+
+    fn complex(c: C) -> Self {
+        Self::Complex(c)
+    }
 }
