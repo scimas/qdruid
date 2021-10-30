4084f916ceeaa0cdbfdc5170da92929bb0c78eb9
Mihir Gadgil <mmgadgil20@gmail.com>
2021-10-30 15:30:44 -0600

    Test post aggregations

--- src/components/post_aggregations.rs
+++ src/components/post_aggregations.rs
@@ -1,3 +1,5 @@
+use std::{error::Error, fmt::Display};
+
 use super::druid_types::DruidType;
 
 pub enum PostAggregator {
@@ -17,7 +19,7 @@ pub enum PostAggregator {
     },
     Constant {
         name: String,
-        value: DruidType,   // must be numeric
+        value: DruidType, // must be numeric
     },
     LongGreatest {
         name: String,
@@ -43,5 +45,45 @@ pub enum PostAggregator {
     HyperUniqueCardinality {
         name: String,
         field_name: String,
+    },
+}
+
+impl PostAggregator {
+    fn constant(name: &str, value: DruidType) -> Result<Self, NonNumericConstant> {
+        match value {
+            DruidType::Long(_) | DruidType::Double(_) | DruidType::Float(_) => Ok(Self::Constant {
+                name: name.into(),
+                value,
+            }),
+            _ => Err(NonNumericConstant {}),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct NonNumericConstant {}
+
+impl Display for NonNumericConstant {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "constant must be numeric")
+    }
+}
+
+impl Error for NonNumericConstant {}
+
+#[cfg(test)]
+mod tests {
+    use std::error::Error;
+
+    use crate::components::druid_types::DruidType;
+
+    use super::{NonNumericConstant, PostAggregator};
+
+    #[test]
+    fn contant_must_be_numeric() -> Result<(), Box<dyn Error>> {
+        match PostAggregator::constant("name", DruidType::String("heh".into())) {
+            Err(NonNumericConstant {}) => Ok(()),
+            _ => Err("did not receive the expected error".into()),
+        }
     }
 }
