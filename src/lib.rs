ce850b49d1cb372172e6f049c0626f7fa4de0dd3
Mihir Gadgil <mmgadgil20@gmail.com>
2021-10-31 20:44:00 -0600

    Expose structs

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,33 @@
+mod async_impl;
 mod components;
 mod queries;
-mod async_impl;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+pub use async_impl::client::Client;
+
+pub use queries::datasource_metadata::DataSourceMetadata;
+pub use queries::groupby::GroupBy;
+pub use queries::scan::Scan;
+pub use queries::search::Search;
+pub use queries::segment_metadata::SegmentMetadata;
+pub use queries::sql::Sql;
+pub use queries::time_boundary::TimeBoundary;
+pub use queries::timeseries::Timeseries;
+pub use queries::topn::TopN;
+pub use queries::Query;
+
+pub use components::aggregations::Aggregator;
+pub use components::context::Context;
+pub use components::dimension_specs::DimensionSpec;
+pub use components::druid_types::{DruidNativeType, DruidSqlType};
+pub use components::extraction_functions::ExtractionFunction;
+pub use components::filters::Filter;
+pub use components::granularities::Granularity;
+pub use components::having_specs::HavingSpec;
+pub use components::intervals::Interval;
+pub use components::limit_specs::LimitSpec;
+pub use components::lookups::Lookup;
+pub use components::post_aggregations::PostAggregator;
+pub use components::search_query_specs::SearchQuerySpec;
+pub use components::to_include::ToInclude;
+pub use components::topn_metric_specs::TopNMetricSpec;
+pub use components::virtual_columns::VirtaulColumn;
