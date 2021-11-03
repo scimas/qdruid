d122a1cf302f1b65e8e0594d6515fcaae005cc4e
Mihir Gadgil <mmgadgil20@gmail.com>
2021-11-02 21:53:59 -0600

    Add constructors for topn metric specs

--- src/components/topn_metric_specs.rs
+++ src/components/topn_metric_specs.rs
@@ -1,13 +1,46 @@
+use super::ordering::InvalidOrderingError;
+
 #[derive(Debug, Clone)]
 pub enum TopNMetricSpec {
     Numeric {
         metric: String,
     },
     Dimension {
-        ordering: Option<String>,
+        ordering: Option<String>, // must be one of "lexicographic", "alphanumeric", "numeric" or "strlen"
         previous_stop: Option<String>,
     },
     Inverted {
         metric: Box<TopNMetricSpec>,
     },
+}
+
+impl TopNMetricSpec {
+    pub fn numeric(metric: String) -> Self {
+        Self::Numeric { metric }
+    }
+
+    pub fn dimension(
+        ordering: Option<String>,
+        previous_stop: Option<String>,
+    ) -> Result<Self, InvalidOrderingError> {
+        if let Some(s) = &ordering {
+            let s = s.to_lowercase();
+            if s != "lexicographic" && s != "alphanumeric" && s != "strlen" && s != "numeric" {
+                return Err(InvalidOrderingError::new(
+                    s,
+                    r#""lexicographic", "alphanumeric", "strlen" or "numeric""#.into(),
+                ));
+            }
+        }
+        Ok(Self::Dimension {
+            ordering,
+            previous_stop,
+        })
+    }
+
+    pub fn inverted(metric: TopNMetricSpec) -> Self {
+        Self::Inverted {
+            metric: Box::new(metric),
+        }
+    }
 }
