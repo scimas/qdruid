c56fae3cd430d0c4ebd29385ae0b7b814e1713c1
Mihir Gadgil <mihir.gadgil@dish.com>
2022-05-13 19:23:00 -0600

    Basic query execution

--- src/async_impl/client.rs
+++ src/async_impl/client.rs
@@ -1,3 +1,10 @@
+use std::error::Error;
+
+use serde::Serialize;
+use serde_json::Value;
+
+use crate::Query;
+
 pub struct Client {
     inner: reqwest::Client,
     native_endpoint: Option<String>,
@@ -5,27 +12,51 @@ pub struct Client {
 }
 
 impl Client {
-    pub fn new(native_endpoint: String, sql_endpoint: String) -> Self {
-        Self {
-            inner: reqwest::Client::new(),
+    pub fn new(native_endpoint: String, sql_endpoint: String) -> Result<Self, Box<dyn Error>> {
+        Ok(Self {
+            inner: reqwest::Client::builder().gzip(true).build()?,
             native_endpoint: Some(native_endpoint),
             sql_endpoint: Some(sql_endpoint),
-        }
+        })
     }
 
-    pub fn native_client(native_endpoint: String) -> Self {
-        Self {
-            inner: reqwest::Client::new(),
+    pub fn native_client(native_endpoint: String) -> Result<Self, Box<dyn Error>> {
+        Ok(Self {
+            inner: reqwest::Client::builder().gzip(true).build()?,
             native_endpoint: Some(native_endpoint),
             sql_endpoint: None,
+        })
+    }
+
+    pub fn sql_client(sql_endpoint: String) -> Result<Self, Box<dyn Error>> {
+        Ok(Self {
+            inner: reqwest::Client::builder().gzip(true).build()?,
+            native_endpoint: None,
+            sql_endpoint: Some(sql_endpoint),
+        })
+    }
+
+    pub async fn execute(&self, query: Query) -> Result<Value, Box<dyn Error>> {
+        match query {
+            Query::Sql(q) => match &self.sql_endpoint {
+                Some(url) => Ok(self.inner.post(url).json(&q).send().await?.json().await?),
+                None => Err("not a SQL client".into()),
+            },
+            Query::DataSourceMetadata(q) => self.native_execute(q).await,
+            Query::GroupBy(q) => self.native_execute(q).await,
+            Query::Scan(q) => self.native_execute(q).await,
+            Query::Search(q) => self.native_execute(q).await,
+            Query::SegmentMetadata(q) => self.native_execute(q).await,
+            Query::TimeBoundary(q) => self.native_execute(q).await,
+            Query::Timeseries(q) => self.native_execute(q).await,
+            Query::TopN(q) => self.native_execute(q).await,
         }
     }
 
-    pub fn sql_client(sql_endpoint: String) -> Self {
-        Self {
-            inner: reqwest::Client::new(),
-            native_endpoint: None,
-            sql_endpoint: Some(sql_endpoint),
+    async fn native_execute<Q: Serialize>(&self, q: Q) -> Result<Value, Box<dyn Error>> {
+        match &self.native_endpoint {
+            Some(url) => Ok(self.inner.post(url).json(&q).send().await?.json().await?),
+            None => Err("not a native query client".into()),
         }
     }
 }
@@ -39,7 +70,8 @@ mod tests {
         let new_client = Client::new(
             "http://localhost:8888/druid/v2".to_string(),
             "http://localhost:8888/druid/v2/sql".to_string(),
-        );
+        )
+        .expect("couldn't create a client");
         let client = Client {
             inner: reqwest::Client::new(),
             native_endpoint: Some("http://localhost:8888/druid/v2".to_string()),
